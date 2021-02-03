dc50ba89e5eeae374b7c8811fdfea00b439f8d92
Denis <denis.zamaro@gmail.com>
2021-02-03 16:12:36 +0200

    Ikelimas

---
+++ Alna_1/Alna_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Alna_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h, m;
+
+            //Valandu ivedimas
+            Console.Write("iveskyte valanda (0-12): ");
+            h = int.Parse(Console.ReadLine());
+
+            if (h < 0 || h > 12)
+            {
+                Console.WriteLine("Neteisingai ivesta valanda!!!");
+                Console.Write("Bandykite dar karta (0-12): ");
+                h = int.Parse(Console.ReadLine());
+            }
+
+            //minuciu ivedimas
+            Console.Write("iveskyte minutes (0-60): ");
+            m = int.Parse(Console.ReadLine());
+            if (m < 0 || m > 60)
+            {
+                Console.WriteLine("Neteisingai ivestos minutes!!!");
+                Console.Write("Bandykite dar karta (0-60): ");
+                m = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"{h} val., {m} min.");
+
+            int trueHours = (m / 12) * 6;           //valandu kampas patikslinimas. kas 12min rodykle pasislenka
+            int minutesAngle = m * 6;
+            int hoursAngle = (h * 30) + trueHours; //tikslus valndu kampas
+
+            
+            // kampo apskaiciavimas
+            int angle = Math.Abs(minutesAngle - hoursAngle);
+
+            if (angle > 180)
+            {
+                int angle1 = 360 - angle;
+                Console.WriteLine($"Maziausias kampas tarp rodykliu yra {angle1}");
+            }
+            else
+            {
+                Console.WriteLine($"Maziausias kampas tarp rodykliu yra {angle}");
+            }
+
+        }
+    }
+}
---
+++ Alna_2/Alna_2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alna_2
+{
+    class Program
+    {
+        //Saku struktura
+        public class Saka
+        {
+            public int id;
+            public List<Saka> sakos;
+        }
+
+        //Naujos Sakos sukurymas
+        static Saka NaujaSaka(int id)
+        {
+            Saka nSaka = new Saka();
+            nSaka.id = id;
+            nSaka.sakos = new List<Saka>();
+            return nSaka;
+        }
+
+        //Sakos gylio paieska
+        static int sakosGylis(Saka sg)
+        {
+            if (sg == null)
+            {
+                return 0;
+            }
+
+            int gylis = 0;
+            foreach (Saka rek in sg.sakos)
+            {
+                gylis = sakosGylis(rek);
+            }
+            return gylis + 1;
+        }
+
+
+        public static void Main(String[] args)
+        {
+            //Medzio duomenys
+
+            Saka saknis = NaujaSaka(1);
+            (saknis.sakos).Add(NaujaSaka(2));
+            (saknis.sakos).Add(NaujaSaka(3));
+            (saknis.sakos[0].sakos).Add(NaujaSaka(4));
+            (saknis.sakos[1].sakos).Add(NaujaSaka(5));
+            (saknis.sakos[1].sakos).Add(NaujaSaka(6));
+            (saknis.sakos[0].sakos[0].sakos).Add(NaujaSaka(7));
+            (saknis.sakos[1].sakos[0].sakos).Add(NaujaSaka(8));
+            (saknis.sakos[1].sakos[1].sakos).Add(NaujaSaka(9));
+            (saknis.sakos[1].sakos[0].sakos[0].sakos).Add(NaujaSaka(10));
+            (saknis.sakos[1].sakos[0].sakos[0].sakos).Add(NaujaSaka(11));
+            (saknis.sakos[1].sakos[1].sakos[0].sakos).Add(NaujaSaka(12));
+            (saknis.sakos[1].sakos[1].sakos[0].sakos).Add(NaujaSaka(13));
+            (saknis.sakos[1].sakos[1].sakos[0].sakos).Add(NaujaSaka(14));
+            (saknis.sakos[1].sakos[1].sakos[0].sakos[2].sakos).Add(NaujaSaka(14));
+
+
+
+            Console.WriteLine($"Medzio gylis yra: {sakosGylis(saknis)}");
+        }
+    }    
+
+ }
+   
